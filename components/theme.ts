d43158746526418dc66d66a22975bc95d5169076
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-01-06 10:55:31 -0800

    Update Server Side Rendering functionality

---
+++ components/theme.ts
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { orange } from '@material-ui/core/colors';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: orange,
+    secondary: orange,
+  }
+});
+
+export default theme;
