28d104fcd505dfe72b7eafb2879d2504f2e120b9
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-06-16 11:50:52 -0700

    Add google analytics head script

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,17 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169638120-1"></script>
+          <script>
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-169638120-1');
+            `}
+          </script>
         </Head>
         <body>
           <Main />
