fa7d399424746bf221886e2b2bbd8e087bd3d5d9
Anton G. Neuhold Jr <agneuhold@gmail.com>
2021-08-29 14:45:56 -0700

    Predictive index Update
    * Switch to Yarn for package management
    
    * Update packages
    
    * Update wording on Hero component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {
   Typography,
   Theme,
-  makeStyles,
   Link,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles((theme: Theme) => ({
   footer: {
