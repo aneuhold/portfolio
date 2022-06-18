afbfbce34e235d23fa493693135989fdbae9236f
Anton G. Neuhold Jr <agneuhold@gmail.com>
2022-06-18 14:34:53 -0700

    Update SocialLink

--- svelte/src/components/SocialLink.svelte
+++ svelte/src/components/SocialLink.svelte
@@ -6,33 +6,34 @@
 <script lang="ts">
   export let url: string;
   export let svgIconPath: string;
-  import IconButton, { Icon } from '@smui/icon-button';
-  import { Svg } from '@smui/common/elements';
 </script>
 
-<div>
-  <a class="svgLink" href={url} rel="noopener noreferrer" target="_blank">
-    <svg class="svgIcon">
-      <path d={svgIconPath} />
-    </svg>
-  </a>
-  <IconButton>
-    <Icon class="svgIcon" component={Svg} viewBox="0 0 24 24">
-      <path d={svgIconPath} />
-    </Icon>
-  </IconButton>
-</div>
+<a class="svgLink" href={url} rel="noopener noreferrer" target="_blank">
+  <svg class="svgIcon">
+    <path d={svgIconPath} />
+  </svg>
+</a>
 
 <style lang="scss">
   @use '../global-styles/theme.scss';
 
+  $icon-size: 24px;
+
   .svgLink {
     padding: 8px;
     border-radius: 50%;
-    color: theme.$color-shadow;
+
+    // The height and the width are set below because for some reason it has
+    // 4px of extra height. Couldn't quite figure out why that was.
+    height: $icon-size;
+    width: $icon-size;
+  }
+  .svgLink:hover {
+    background-color: theme.$color-shadow;
   }
 
   .svgIcon {
+    border-radius: 50%;
     width: 1em;
     height: 1em;
     color: theme.$color-primary;
