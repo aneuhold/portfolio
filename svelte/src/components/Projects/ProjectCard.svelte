adaf09e9b76a23bed17b613005d90d25e95bbc87
Anton G. Neuhold Jr <agneuhold@gmail.com>
2022-06-18 16:53:55 -0700

    Add TextButton component

--- svelte/src/components/Projects/ProjectCard.svelte
+++ svelte/src/components/Projects/ProjectCard.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import TextButton from '../TextButton.svelte';
+
   export let projectName: string;
   export let imgUrl: string;
   export let imgAlt: string;
@@ -11,6 +13,9 @@
     <h3 class="header-4">{projectName}</h3>
     <p>{info}</p>
   </div>
+  <div class="footer">
+    <TextButton text="test" url="www.google.com" />
+  </div>
 </div>
 
 <style lang="scss">
@@ -19,15 +24,24 @@
   $border-radius: calc(theme.$standard-spacing / 2);
 
   .projectCard {
-    flex-grow: 1;
     display: flex;
     flex-direction: column;
+    // Crazy box-shadow!! Not really sure what is going on here. Copy pasted
+    // from React material UI
     box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%),
       0px 1px 3px 0px rgb(0 0 0 / 12%);
     border-radius: $border-radius;
   }
+  h3 {
+    // Kind of a weird bottom margin, this is what Material UI has though.
+    margin: 0 0 0.35em 0;
+  }
+  p {
+    margin: 0;
+  }
   .media {
     display: block;
+    // padding-top is how tall the image is. This is currently a 16:9 ratio.
     padding-top: 56.25%;
     background-size: cover;
     background-position: center;
@@ -36,9 +50,12 @@
   }
   .textContent {
     text-align: left;
+    height: 100%;
     padding: calc(theme.$standard-spacing * 2);
   }
   .footer {
-    padding: calc(theme.$standard-spacing * 2);
+    padding: theme.$standard-spacing;
+    display: flex;
+    flex-direction: row;
   }
 </style>
---
+++ svelte/src/components/TextButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let text: string;
+  export let url: string;
+</script>
+
+<a class="button-text" href={url}>
+  {text}
+</a>
+
+<style lang="scss">
+  @use '../global-styles/theme.scss';
+  a {
+    display: flex;
+    flex-direction: row;
+    padding: 4px 5px;
+    border-radius: calc(theme.$standard-spacing / 2);
+    text-decoration: none;
+    justify-content: center;
+    min-width: 64px;
+    color: theme.$color-primary;
+  }
+
+  a:hover {
+    background-color: theme.$color-primary-100;
+  }
+</style>
