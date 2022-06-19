739b5ba34e2feec8b314b63d3309d50a4c222116
Anton G. Neuhold Jr <agneuhold@gmail.com>
2022-06-19 13:52:31 -0700

    Add a basic working test

--- svelte/src/routes/index.spec.ts
+++ svelte/src/routes/index.spec.ts
@@ -1,9 +1,9 @@
 import { render } from '@testing-library/svelte';
 import index from './index.svelte';
 
-it('it works', async () => {
+it('it works at a basic level', async () => {
   const { getByText } = render(index);
 
-  const titleText = getByText('Anton');
+  const titleText = getByText('Anton (Tony) Neuhold');
   expect(titleText).toBeDefined();
 });
