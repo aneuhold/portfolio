53d83d17d5dfdf26afea49ea856a472c48b664de
Anton G Neuhold Jr <agneuhold@gmail.com>
2025-09-01 09:52:51 -0700

    Fix bug

--- shared/config/projects.ts
+++ shared/config/projects.ts
@@ -42,7 +42,7 @@ const projects = {
     name: 'Next.js Invoices App',
     heading: 'Next.js Invoices App',
     info: 'A Next.js 15 course project that is an invoice app. It uses a backend with PostgreSQL and Neon + Vercel. See repo for login info.',
-    codeLink: 'https://github.com/aneuhold/main-scripts',
+    codeLink: 'https://github.com/aneuhold/nextjs-15-course',
     demoLink: 'https://nextjs-15-course-xi.vercel.app/',
     thumbnailDescription: 'Next.js 15 Invoices App thumbnail'
   },
